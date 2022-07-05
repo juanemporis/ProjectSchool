1a97adecd5623285ba4172eeb3100a7bed329f09
juan castañeda <juanemporis123>
2022-07-05 10:38:51 -0500

    Initial Commit

---
+++ ProjectSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectSchool
+//
+//  Created by wendy manrique on 5/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
