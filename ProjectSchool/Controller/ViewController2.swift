6a182f37933f61a83cba9de576227d7dcd2b364f
juan castañeda <juanemporis123>
2022-07-06 17:59:46 -0500

    Se añadió el delegase al tableView y se añadio la función didSelectRowAr para que al seleccionar una celda nos muestre la siguiente ViewController3

--- ProjectSchool/Controller/ViewController2.swift
+++ ProjectSchool/Controller/ViewController2.swift
@@ -31,25 +31,36 @@ class ViewController2: UIViewController {
         super.viewDidLoad()
         //Data source es un protocolo delegado que se usa en las tableView
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.tableFooterView = UIView()
         
     }
     
 }
-extension ViewController2: UITableViewDataSource {
+extension ViewController2: UITableViewDelegate,UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myLanguage.count
-    }
-
+        
+}
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
     var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
     if cell == nil{
         cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
         cell?.accessoryType = .disclosureIndicator
         
     }
-    
+
     cell!.textLabel?.text = myLanguage[indexPath.row]
     return cell!
  }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "nextPage", sender: nil)
+       /*let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc3 = storyboard.instantiateViewController(withIdentifier: "nextPage")
+        navigationController?.pushViewController(vc3, animated: true)*/
+        
+    }
 }
+
--- ProjectSchool/Controller/ViewController3.swift
+++ ProjectSchool/Controller/ViewController3.swift
@@ -19,7 +19,7 @@ class ViewController3: UIViewController {
         super.viewDidLoad()
         tableView3.dataSource = self
         tableView3.tableFooterView = UIView()
-
+        
         
     }
     
@@ -29,13 +29,17 @@ extension ViewController3: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myGoal.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView3.dequeueReusableCell(withIdentifier: "myCell2")
         if cell == nil {
             cell = UITableViewCell(style: .default, reuseIdentifier: "myCell2")
             cell?.accessoryType = .detailButton
+            
         }
+    
         cell!.textLabel?.text = myGoal[indexPath.row]
         return cell!
     }
+
 }
